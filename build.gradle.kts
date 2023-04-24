205a93f7efc9eae683140db155479ddbcdc38f4d
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-04-24 20:11:26 +0800

    config(gradle): add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,67 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
+import io.gitlab.arturbosch.detekt.report.ReportMergeTask
+
 plugins {
     id("com.android.application") version "8.1.0-beta01" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("io.gitlab.arturbosch.detekt").version("1.23.0-RC2")
+}
+
+val detektReportMergeSarif by tasks.registering(ReportMergeTask::class) {
+    output.set(rootProject.layout.buildDirectory.file("reports/detekt/merge.sarif"))
+}
+
+tasks.withType<Detekt>().configureEach {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        txt.required.set(true)
+        sarif.required.set(true)
+        md.required.set(true)
+    }
+}
+
+allprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    dependencies {
+        detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.22.0")
+    }
+
+    detekt {
+        parallel = true
+        ignoreFailures = true
+        buildUponDefaultConfig = true
+
+        source = objects.fileCollection().from(
+            DetektExtension.DEFAULT_SRC_DIR_JAVA,
+            DetektExtension.DEFAULT_TEST_SRC_DIR_JAVA,
+            DetektExtension.DEFAULT_SRC_DIR_KOTLIN,
+            DetektExtension.DEFAULT_TEST_SRC_DIR_KOTLIN,
+        )
+    }
+
+    tasks.withType<Detekt> {
+        basePath = rootProject.projectDir.absolutePath
+        jvmTarget = "1.8"
+
+        reports {
+            xml.required.set(true)
+            html.required.set(true)
+            sarif.required.set(true)
+        }
+
+        finalizedBy(detektReportMergeSarif)
+    }
+
+    // Merge detekt report into sarif file for CodeQL scanning
+    detektReportMergeSarif.configure {
+        input.from(tasks.withType<Detekt>().map { it.sarifReportFile })
+    }
+
+    tasks.withType<DetektCreateBaselineTask>().configureEach {
+        jvmTarget = "1.8"
+    }
 }
