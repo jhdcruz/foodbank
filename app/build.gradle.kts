7ca9bcd1a83e1b4ffb83e1369d0f44d7309efd98
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-04-24 22:09:10 +0800

    config(gradle): shrink resources on release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,8 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
