383e32857712fff2cf2849dd52cdc43a5d835270
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-04-25 09:34:47 +0800

    config(gradle): gradle enterprise build scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,5 +29,21 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Food Bank"
+plugins {
+    `gradle-enterprise`
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+rootProject.name = "foodbank"
 include(":app")
