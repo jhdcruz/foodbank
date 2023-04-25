6999f2dbadcd05043c6e781dfee747a183bb77bd
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-04-25 10:08:02 +0800

    chore: trunk fmt

--- app/src/main/kotlin/com/ptech/foodbank/ui/home/HomeViewModel.kt
+++ app/src/main/kotlin/com/ptech/foodbank/ui/home/HomeViewModel.kt
@@ -1,13 +1,7 @@
+@file:Suppress("unused")
+
 package com.ptech.foodbank.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
-}
+class HomeViewModel : ViewModel()
