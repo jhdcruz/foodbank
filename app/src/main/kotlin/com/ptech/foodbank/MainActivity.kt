c835868a9e0431c265117e563ba0b42e99561574
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-04-25 14:38:03 +0800

    feat: user location tracking

--- app/src/main/kotlin/com/ptech/foodbank/MainActivity.kt
+++ app/src/main/kotlin/com/ptech/foodbank/MainActivity.kt
@@ -2,9 +2,8 @@ package com.ptech.foodbank
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ptech.foodbank.databinding.ActivityMainBinding
@@ -20,19 +19,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
+        val navHostFragment: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home,
-                R.id.navigation_dashboard,
-                R.id.navigation_notifications,
-            ),
-        )
+        val navController: NavController = navHostFragment.navController
 
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
--- app/src/main/kotlin/com/ptech/foodbank/ui/home/HomeFragment.kt
+++ app/src/main/kotlin/com/ptech/foodbank/ui/home/HomeFragment.kt
@@ -4,18 +4,60 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import com.mapbox.android.gestures.MoveGestureDetector
+import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.PuckBearingSource
+import com.mapbox.maps.plugin.gestures.OnMoveListener
+import com.mapbox.maps.plugin.gestures.gestures
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
+import com.mapbox.maps.plugin.locationcomponent.location2
 import com.ptech.foodbank.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-    private var mapView: MapView? = null
-
     private val binding get() = _binding!!
 
+    private lateinit var mapView: MapView
+
+    // Get the user's location as coordinates
+    private val onIndicatorBearingChangedListener = OnIndicatorBearingChangedListener {
+        mapView.getMapboxMap().setCamera(
+            CameraOptions.Builder()
+                .bearing(it)
+                .build(),
+        )
+    }
+    private val onIndicatorPositionChangedListener = OnIndicatorPositionChangedListener {
+        mapView.getMapboxMap().setCamera(
+            CameraOptions.Builder()
+                .center(it)
+                .build(),
+        )
+        mapView.gestures.focalPoint = mapView.getMapboxMap().pixelForCoordinate(it)
+    }
+
+    private val onMoveListener = object : OnMoveListener {
+        override fun onMoveBegin(detector: MoveGestureDetector) {
+            onCameraTrackingDismissed()
+        }
+
+        override fun onMove(detector: MoveGestureDetector): Boolean {
+            return false
+        }
+
+        @Suppress("EmptyFunctionBlock")
+        override fun onMoveEnd(detector: MoveGestureDetector) {
+        }
+    }
+
+    @Suppress("MagicNumber")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,13 +68,58 @@ class HomeFragment : Fragment() {
 
         // setup mapbox maps
         mapView = binding.mapView
-        mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
+        // setup mapbox map
+        mapView.getMapboxMap().loadStyleUri(
+            Style.MAPBOX_STREETS,
+        ) {
+            mapView.location2.updateSettings {
+                enabled = true
+                pulsingEnabled = true
+            }
+            mapView.gestures.addOnMoveListener(onMoveListener)
+        }
+
+        // request permission
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission(),
+        ) { isGranted: Boolean ->
+            if (!isGranted) {
+                val text = "We couldn't get your location"
+                Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+            } else {
+                // get and mark current location
+                mapView.location2.puckBearingSource = PuckBearingSource.HEADING
+                mapView.location2.puckBearingSource = PuckBearingSource.COURSE
+            }
+        }
+
+        // Pass the user's location to camera
+        mapView.location2.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+        mapView.location2.addOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
 
         return root
+    }
+
+    private fun onCameraTrackingDismissed() {
+        mapView.location2.removeOnIndicatorPositionChangedListener(
+            onIndicatorPositionChangedListener,
+        )
+        mapView.location2.removeOnIndicatorBearingChangedListener(
+            onIndicatorBearingChangedListener,
+        )
+        mapView.gestures.removeOnMoveListener(onMoveListener)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+
+        mapView.location2.removeOnIndicatorBearingChangedListener(
+            onIndicatorBearingChangedListener,
+        )
+        mapView.location2.removeOnIndicatorPositionChangedListener(
+            onIndicatorPositionChangedListener,
+        )
+        mapView.gestures.removeOnMoveListener(onMoveListener)
     }
 }
