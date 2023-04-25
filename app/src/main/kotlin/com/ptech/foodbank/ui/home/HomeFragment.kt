99179133ccf14b1079737c803f197c3040e23ef0
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-04-25 09:16:01 +0800

    chore: trunk fmt

--- app/src/main/kotlin/com/ptech/foodbank/ui/home/HomeFragment.kt
+++ app/src/main/kotlin/com/ptech/foodbank/ui/home/HomeFragment.kt
@@ -19,7 +19,7 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
