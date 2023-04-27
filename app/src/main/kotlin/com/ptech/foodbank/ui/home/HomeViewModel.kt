c8786e86f6f030f852d021988b067d33a35faf80
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-04-27 23:36:56 +0800

    feat: food bank card list

--- app/src/main/kotlin/com/ptech/foodbank/ui/home/HomeViewModel.kt
+++ app/src/main/kotlin/com/ptech/foodbank/ui/home/HomeViewModel.kt
@@ -1,7 +1,16 @@
-@file:Suppress("unused")
-
 package com.ptech.foodbank.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.google.firebase.firestore.QuerySnapshot
+import com.ptech.foodbank.db.DbFactory
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.tasks.await
 
-class HomeViewModel : ViewModel()
+class HomeViewModel : ViewModel() {
+    private val db = DbFactory()
+
+    val savedBanks = flow<QuerySnapshot> {
+        emit(db.getBanks().get().await())
+    }.asLiveData()
+}
