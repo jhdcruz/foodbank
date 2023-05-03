c36cda5c7c7561925762b4fa190986447e6eff8a
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-05-03 14:18:22 +0800

    core(db): remove redundant persistence setting
    Offline persistence is enabled by default

--- app/src/main/kotlin/com/ptech/foodbank/db/FirestoreFactory.kt
+++ app/src/main/kotlin/com/ptech/foodbank/db/FirestoreFactory.kt
@@ -2,17 +2,9 @@ package com.ptech.foodbank.db
 
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestoreSettings
 
 internal class FirestoreFactory {
     private val db = FirebaseFirestore.getInstance()
-
-    init {
-        // allow local persistence (offline)
-        db.firestoreSettings = FirebaseFirestoreSettings.Builder()
-            .setPersistenceEnabled(true)
-            .build()
-    }
 
     fun getBanks(): CollectionReference {
         return db.collection("banks")
