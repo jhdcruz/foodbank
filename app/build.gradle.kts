7d817b08669d6a26dff99aa7e343d7a933338eaf
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-05-03 16:42:11 +0800

    gradle: use firebase bom for versioning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,8 @@ android {
 }
 
 dependencies {
+    implementation(platform(libs.firebase.bom))
+
     implementation(libs.androidx.core)
     implementation(libs.appcompat)
     implementation(libs.material.ui)
