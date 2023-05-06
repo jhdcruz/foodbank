973097b6f3de5d42bd0770f4023235fb8c55dbd5
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-05-06 17:14:21 +0800

    feat(home): logged-in user avatar

--- app/src/main/kotlin/com/ptech/foodbank/ui/home/HomeFragment.kt
+++ app/src/main/kotlin/com/ptech/foodbank/ui/home/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.ptech.foodbank.ui.home
 import android.app.Activity.RESULT_OK
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
@@ -10,6 +11,8 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
@@ -19,6 +22,7 @@ import com.ptech.foodbank.R
 import com.ptech.foodbank.data.Bank
 import com.ptech.foodbank.databinding.FragmentHomeBinding
 import com.ptech.foodbank.utils.Auth.getAuth
+import com.ptech.foodbank.utils.Coil
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -29,9 +33,12 @@ class HomeFragment : Fragment() {
     private lateinit var refresher: SwipeRefreshLayout
     private lateinit var loader: CircularProgressIndicator
     private lateinit var searchBar: SearchBar
+    private lateinit var avatar: MenuItem
 
     private var banksRecyclerView: RecyclerView? = null
     private var banksList: List<Bank>? = null
+
+    private var currentUser = getAuth.currentUser
 
     private val signInLauncher = registerForActivityResult(
         FirebaseAuthUIActivityResultContract()
@@ -62,6 +69,7 @@ class HomeFragment : Fragment() {
         // search bar
         searchBar = binding.searchBar
         searchBar.inflateMenu(R.menu.search_menu)
+        avatar = searchBar.menu.findItem(R.id.user)
 
         // pull-to-refresh feature
         refresher = binding.swipeRefresh
@@ -90,6 +98,7 @@ class HomeFragment : Fragment() {
             }
         }
 
+        loadAvatar()
         getData()
     }
 
@@ -107,12 +116,29 @@ class HomeFragment : Fragment() {
     }
 
     private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
-        val response = result.idpResponse
-
         if (result.resultCode == RESULT_OK) {
-            // Successfully signed in
-            val user = getAuth.currentUser
+            currentUser = getAuth.currentUser
+
+            loadAvatar()
         }
+    }
+
+    private fun loadAvatar() {
+        val loader = Coil.imageLoader(requireContext())
+
+        val request =
+            ImageRequest.Builder(requireContext())
+                .data(currentUser?.photoUrl)
+                .target(
+                    onSuccess = { result ->
+                        avatar.icon = result
+                    },
+                )
+                .crossfade(true)
+                .transformations(CircleCropTransformation())
+                .build()
+
+        loader.enqueue(request)
     }
 
     override fun onDestroyView() {
